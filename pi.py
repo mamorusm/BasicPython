4d6685fe80f55c48ace511bb90252e5749d5218b
mamorusm <mamo83sk@gmail.com>
2023-08-23 08:18:16 +0900

    pi.py

--- pi.py
+++ pi.py
@@ -4,4 +4,8 @@ text = """
 """
 
 # TODO
+numbers=(list(map(len, ["How","I","want", "a", "drink","alcoholic", "of", "course"," after", "the", "heavy", "chapters", "involving", "quantum", "mechanics", "All", "of", "thy", "geometry"," Herr", "Planck", "is", "fairly", "hard"]))
+)
+print(int("".join(map(str,numbers))))
+# リストの数字をつなげて表示するコマンド
 
