89228c4791003b3e3f6324913ffbf1eac40654f3
mamorusm <mamo83sk@gmail.com>
2023-08-30 10:07:58 +0900

    関数(修正版)

--- prime_number.py
+++ prime_number.py
@@ -1,20 +1,20 @@
 # TODO
+def prime(n):
+        m=2
+        while n>m:
+            if n%m==0:
+                return(False)
+                break
+            m+=1
+        if n==m:
+            return(True)
+        if n==1:
+            return(False)
 try:
     n=int(input('n:'))
     if n<=0:
-         print('自然数を入力してください')
+        print('自然数を入力してください')
     if n>0:
-        def prime(n):
-                m=2
-                while n>m:
-                    if n%m==0:
-                        return(1==2)
-                        break
-                    m+=1
-                if n==m:
-                    return(1==1)
-                if n==1:
-                    return(1==2)
         print(prime(n))
 except(ValueError):
     print('自然数を入力してください')
